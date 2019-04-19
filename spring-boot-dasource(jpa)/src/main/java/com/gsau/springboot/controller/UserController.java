34d321e8168724f6c1ad4a2743e3d5a1b2235534
WangGuoQing <1456065693@qq.com>
2019-04-19 14:11:38 +0800

    jpa方式访问数据库的方式

---
+++ spring-boot-dasource(jpa)/src/main/java/com/gsau/springboot/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.gsau.springboot.controller;
+
+import com.gsau.springboot.entity.User;
+import com.gsau.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author WangGuoQing
+ * @date 2019/4/19 14:05
+ * @Desc 测试JPA访问
+ */
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public User getUser(@PathVariable("id") Integer id){
+        User user = userRepository.findOne(id);
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User insertUser(User user){
+        User save = userRepository.save(user);
+        return save;
+    }
+
+}
