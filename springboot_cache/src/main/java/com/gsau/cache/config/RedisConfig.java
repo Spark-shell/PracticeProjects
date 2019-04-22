254d958211ee074f3f7126353a53e826d7f1c634
WangGuoQing <1456065693@qq.com>
2019-04-22 16:14:38 +0800

    springboot 1.x版本和2.x版本定制CacheManager的不同

--- springboot_cache/src/main/java/com/gsau/cache/config/RedisConfig.java
+++ springboot_cache/src/main/java/com/gsau/cache/config/RedisConfig.java
@@ -67,24 +67,8 @@ public class RedisConfig {
      *         return cacheManager;
      *     }    //缓存管理
      *
-     * 然而在2.x版本中，这个代码直接报错，原因是RedisCacheManager取消了1.0版本中的public RedisCacheManager(RedisOperations redisOperations)的这个构造方法，
+     *      然而在2.x版本中，这个代码直接报错，原因是RedisCacheManager取消了1.0版本中的public RedisCacheManager(RedisOperations redisOperations)的这个构造方法，
      * 所以我们无法再用RedisTemplate作为参数来自定义CacheManager。
-     *
-     *     @Bean
-     *     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-     *         //初始化一个RedisCacheWriter
-     *         RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
-     *         //设置CacheManager的值序列化方式为json序列化
-     *         RedisSerializer<Object> jsonSerializer = new GenericJackson2JsonRedisSerializer();
-     *         RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair
-     *                                                     .fromSerializer(jsonSerializer);
-     *         RedisCacheConfiguration defaultCacheConfig=RedisCacheConfiguration.defaultCacheConfig()
-     *                                                     .serializeValuesWith(pair);
-     *         //设置默认超过期时间是30秒
-     *         defaultCacheConfig.entryTtl(Duration.ofSeconds(30));
-     *         //初始化RedisCacheManager
-     *         return new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
-     *     }
      * @param factory
      * @return
      */
@@ -99,7 +83,7 @@ public class RedisConfig {
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
 
-        // 配置序列化（解决乱码的问题）
+        // 配置序列化（解决乱码的问题）      --->RedisCacheConfiguration属性配置
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
