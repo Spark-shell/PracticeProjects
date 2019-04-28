2d25e5b126f4956df6faaf419165fa426523235f
WangGuoQing <1456065693@qq.com>
2019-04-28 11:07:38 +0800

    idea创建多模块项目

---
+++ spring_cloud_learn/api_8081/src/main/java/com/gsau/api_8081/Api8081Application.java
@@ -0,0 +1,12 @@
+package com.gsau.api_8081;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api8081Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Api8081Application.class, args);
+    }
+
+}
---
+++ spring_cloud_learn/provider_dept_8082/src/main/java/com/gsau/providerdept88082/ProviderDept88082Application.java
@@ -0,0 +1,14 @@
+package com.gsau.providerdept88082;
+
+import com.gsau.api_8081.entities.Department;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderDept88082Application {
+
+    public static void main(String[] args) {
+        Department department = new Department();
+        // SpringApplication.run(ProviderDept88082Application.class, args);
+    }
+
+}
