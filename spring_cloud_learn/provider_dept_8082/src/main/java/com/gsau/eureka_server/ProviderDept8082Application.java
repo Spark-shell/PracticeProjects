dd47dddf1b557f7646c89287177b81e3b876b14a
WangGuoQing <1456065693@qq.com>
2019-04-28 16:15:50 +0800

    provider_dept_8082作为Eureka客户端入注Server端

--- spring_cloud_learn/provider_dept_8082/src/main/java/com/gsau/eureka_server/ProviderDept8082Application.java
+++ spring_cloud_learn/provider_dept_8082/src/main/java/com/gsau/eureka_server/ProviderDept8082Application.java
@@ -2,17 +2,17 @@ package com.gsau.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author WangGuoQing
  * @date 2019/4/28 12:15
- * @Desc 
+ * @Desc @EnableEurekaClient 帮助服务启动都自动注册近Eureka Server中
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class ProviderDept8082Application {
-
     public static void main(String[] args) {
-        // Department department = new Department();
         SpringApplication.run(ProviderDept8082Application.class, args);
     }
 
