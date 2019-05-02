31a46cdfe143f326c0db485753d1c18bdac839b2
WangGuoQing <1456065693@qq.com>
2019-05-02 20:40:27 +0800

    函数和过程的关系

--- scala_learn/demo_02/src/main/scala/com/gsau/Procedure_Function.scala
+++ scala_learn/demo_02/src/main/scala/com/gsau/Procedure_Function.scala
@@ -8,12 +8,27 @@ package com.gsau
   */
 object Procedure_Function {
     def main(args: Array[String]): Unit = {
-
+        lazy val sum = lazyFunction(2, 3)
+        println("---------------")
+        println(sum)
     }
     /**
       * 定义一个过程
       */
     def func1(): Unit = {
         println("过程！")
+    }
+    /**
+      *     当函数返回值被声明为lazy时，函数的执行将会被推迟，知道首次用其结果的时候函数才会执行，这种函数被称作惰性函数
+      * 注意：
+      *     1.lazy不能修饰var 类型的变量
+      *     2.不单单是在函数调用时，加了lazy会导致函数的延迟执行，同样在声明一个常量的时候加了lazy，那么变量值的分配也会被延迟
+      * @param n1
+      * @param n2
+      * @return
+      */
+    def lazyFunction(n1: Int, n2: Int): Int = {
+        println("lazyFunction函数执行.....")
+        return n1 + n2
     }
 }
