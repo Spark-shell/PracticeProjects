7f85dce7d59b663c3a7363ccbcf8e1fe0f30729b
WangGuoQing <1456065693@qq.com>
2019-05-02 20:51:11 +0800

    scala异常处理

--- scala_learn/demo_02/src/main/scala/com/gsau/ExceptionProcess.scala
+++ scala_learn/demo_02/src/main/scala/com/gsau/ExceptionProcess.scala
@@ -1,5 +1,25 @@
 package com.gsau
-
 object ExceptionProcess {
-
+    def main(args: Array[String]): Unit = {
+        try {
+            caluteNum(0, 0)
+        } catch {
+            case exception: ArithmeticException => {
+                println("捕获到0作为除数的异常")
+            }
+        } finally {
+            println("程序结束....")
+        }
+    }
+    /**
+      * 计算
+      *
+      * @param num1
+      * @param num2
+      * @return
+      */
+    def caluteNum(num1: Int, num2: Int): Any = {
+        var result = num1 / num2
+        result
+    }
 }
