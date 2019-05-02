850a071d631928ed3cffefccde16b9702533c54d
WangGuoQing <1456065693@qq.com>
2019-05-02 20:24:54 +0800

    函数和过程的关系

--- scala_learn/demo_02/src/main/scala/com/gsau/Procedure_Function.scala
+++ scala_learn/demo_02/src/main/scala/com/gsau/Procedure_Function.scala
@@ -3,8 +3,17 @@ package com.gsau
   * @author WangGuoQing
   * @date 2019/5/2 20:05
   * @Desc 函数（function）和过程（procedure）的区别
-  *      
+  *      1.将函数的返回值类型是Unit的函数称之为过程
+  *
   */
 object Procedure_Function {
+    def main(args: Array[String]): Unit = {
 
+    }
+    /**
+      * 定义一个过程
+      */
+    def func1(): Unit = {
+        println("过程！")
+    }
 }
