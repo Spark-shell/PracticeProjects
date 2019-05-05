0d9d4d4b1b86c4ca4f9eff4e745483f6ae1f8b62
WangGuoQing <1456065693@qq.com>
2019-05-05 10:13:51 +0800

    定义接受简单函数作为参数的方法，和闭包问题

--- scala_learn/demo_02/src/main/scala/com/gsau/ScalaClosure.scala
+++ scala_learn/demo_02/src/main/scala/com/gsau/ScalaClosure.scala
@@ -5,5 +5,25 @@ package com.gsau
   * @Desc 定义接受简单函数作为参数的方法，和闭包问题
   */
 object ScalaClosure {
-
+    def main(args: Array[String]): Unit = {
+        method1(x=>println(x+"====method1======"),"测试参数是函数的方法")
+        mothod2((x,y) => x >= y,1,2)
+    }
+    /**
+      * 参数接受一个函数，这个函数要求输入一个Int值不返回
+      * @param f
+      */
+    def method1(f:String=>Unit,parameter1:String): Unit ={
+       f(parameter1)
+    }
+    /**
+      * 接受一个函数作为参数，这个函数的输入值是（Int,Int）返回值的Boolean
+      * @param f1
+      * @param num1
+      * @param num2
+      */
+    def mothod2(f1:(Int,Int)=>Boolean,num1:Int,num2:Int): Unit ={
+        var flag:Boolean=f1(num1,num2)
+        println(s"==返回的是===$flag")
+    }
 }
