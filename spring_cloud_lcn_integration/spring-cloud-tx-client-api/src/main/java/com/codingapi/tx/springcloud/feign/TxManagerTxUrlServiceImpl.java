85ecd626ae8cffc638d8f9957494ff4e6de232b9
WangGuoQing <1456065693@qq.com>
2019-05-09 13:20:57 +0800

    分布式事务整合

--- spring_cloud_lcn_integration/spring-cloud-tx-client-api/src/main/java/com/codingapi/tx/springcloud/feign/TxManagerTxUrlServiceImpl.java
+++ spring_cloud_lcn_integration/spring-cloud-tx-client-api/src/main/java/com/codingapi/tx/springcloud/feign/TxManagerTxUrlServiceImpl.java
@@ -1,21 +1,22 @@
-package com.gsau.tx_manager.service;
+package com.codingapi.tx.springcloud.feign;
 
 import com.codingapi.tx.config.service.TxManagerTxUrlService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+/**
+ * Created by liuliang on 2018/10/10.
+ */
 @Service
-public class TxUrlService implements TxManagerTxUrlService {
+public class TxManagerTxUrlServiceImpl implements TxManagerTxUrlService{
+
+
     @Value("${tm.manager.url}")
     private String url;
 
-    /**
-     * 返回访问事务协调器的URL
-     * @return
-     */
     @Override
     public String getTxUrl() {
-        //"http://192.168.199.56:8888/tx/manager/"
+        System.out.println("load tm.manager.url ");
         return url;
     }
 }
