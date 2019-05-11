a9783501ad83ce8026d1363ccac3bf6ed290e73e
WangGuoQing <1456065693@qq.com>
2019-05-11 09:22:03 +0800

    修改启动地址

--- spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/PortalApplication.java
+++ spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/PortalApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 public class PortalApplication {
     public static void main(String[] args) {
