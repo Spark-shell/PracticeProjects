9b1115d66440c35ec1a961f499306c4e42e28187
BOFeng <1456065693@qq.com>
2019-05-13 11:47:23 +0800

    modify

--- spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/feign_service/PortalInventoryService.java
+++ spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/feign_service/PortalInventoryService.java
@@ -3,6 +3,6 @@ package com.gsau.portal.feign_service;
 import com.gsau.inventory_sersvice.services.InventoryService;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient("SPRING-CLOUD-INVENTORY-7002")
+@FeignClient("SPRING-CLOUD-INVENTORY-7002-DEV")
 public interface PortalInventoryService extends InventoryService {
 }
--- spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/feign_service/PortalOrderService.java
+++ spring_cloud_lcn_integration/spring_cloud_portal/src/main/java/com/gsau/portal/feign_service/PortalOrderService.java
@@ -3,7 +3,7 @@ package com.gsau.portal.feign_service;
 import com.gsau.order_sersvice.services.OrderService;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-@FeignClient("SPRING-CLOUD-ORDER-7003")
+@FeignClient("SPRING-CLOUD-ORDER-7003-DEV")
 public interface PortalOrderService extends OrderService {
 
 }
