a71715652ebbf6ec66ce851411ec64c951103f33
WangGuoQing <1456065693@qq.com>
2019-05-15 09:55:27 +0800

    添加EasyUI

---
+++ spring_cloud_lcn_integration/spring_cloud_order/src/main/java/com/gsau/order/mapper/UserInfoMapper.java
@@ -0,0 +1,28 @@
+package com.gsau.order.mapper;
+
+import com.gsau.order_sersvice.projo.po.UserInfo;
+
+/**
+ * @author WangGuoQing
+ * @date 2019/5/15 9:22
+ * @Desc 用户信息
+ */
+public interface UserInfoMapper {
+    /**
+     * 插入
+     * @param userInfo
+     */
+    void insertUserInfo(UserInfo userInfo);
+
+    /**
+     *  根据useridc查询
+     * @param userid
+     */
+    void findByUserId(int userid);
+
+    /**
+     * 电话号查询
+     * @param usertel
+     */
+    void findByUserTel(int usertel);
+}
