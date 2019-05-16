3ce982f71eaa288d90a753dba1944162183b3c0c
Administrator <1456065693@qq.com>
2019-05-16 16:35:35 +0800

    科目管理分割到Inventory服务上去

---
+++ spring_cloud_lcn_integration/spring_cloud_inventory/src/main/java/com/gsau/Inventory/controller/InventorySubjectController.java
@@ -0,0 +1,12 @@
+package com.gsau.Inventory.controller;
+
+import com.gsau.inventory_sersvice.services.InventorySubjectService;
+/**
+ * @Description: 
+ * @Date: 2019/5/16 16:35
+ * @author: WangGuoQing
+ * @version: 1.0
+ */
+public class InventorySubjectController implements InventorySubjectService {
+    
+}
---
+++ spring_cloud_lcn_integration/spring_cloud_inventory_service/src/main/java/com/gsau/inventory_sersvice/services/InventorySubjectService.java
@@ -0,0 +1,16 @@
+package com.gsau.inventory_sersvice.services;
+
+import com.gsau.inventory_sersvice.projo.Inventory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Description:
+ * @Date: 2019/5/16 16:33
+ * @author: WangGuoQing
+ * @version: 1.0
+ */
+public interface InventorySubjectService {
+
+}
