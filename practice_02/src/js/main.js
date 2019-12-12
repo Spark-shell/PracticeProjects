810192f919f1120ae0ccf07ed996fa4d3afdf73d
WangGuoQing <1456065693@qq.com>
2019-12-12 21:26:10 +0800

    Vue启动成功

--- practice_02/src/js/main.js
+++ practice_02/src/js/main.js
@@ -1,14 +1,36 @@
 const {getInfo} =require('./page.js')
-//1.安装vue   npm install vue --save
-//2.引入vue
-import Vue from 'vue'
 console.log(getInfo())
+//依赖css文件
 require('../css/style.css')
 //依赖less文件   npm install less-loader less --save-dev
 require('../css/special.less')
+//1.安装vue   npm install vue --save
+//2.引入vue
+import Vue from 'vue'
+const App={
+    template:`
+      <div>
+            <h2> {{ msg }}</h2>
+            <button @click="btnClick">点我啊！</button>
+            <h2> {{ num }}</h2>
+       </div>
+    `,
+    data(){
+        return {
+            num:0,
+            msg:"世界你好！！！"
+        }
+    },
+    methods:{
+        btnClick(){
+            this.num ++
+        }
+    }
+}
 const app=new Vue({
     el:'#app',
-    data:{
-        msg:"HelloWebPack!!!"
+     template: '<App/>',
+    components:{
+        App
     }
 })
