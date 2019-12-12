0d7545f50294173573b1c513c27baf7a02dd67b6
WangGuoQing <1456065693@qq.com>
2019-12-12 21:05:18 +0800

    Vue启动成功

--- practice_02/src/js/main.js
+++ practice_02/src/js/main.js
@@ -1,5 +1,14 @@
 const {getInfo} =require('./page.js')
+//1.安装vue   npm install vue --save
+//2.引入vue
+import Vue from 'vue'
 console.log(getInfo())
 require('../css/style.css')
 //依赖less文件   npm install less-loader less --save-dev
 require('../css/special.less')
+const app=new Vue({
+    el:'#app',
+    data:{
+        msg:"HelloWebPack!!!"
+    }
+})
