2efcdd5da8d7aa92b357d1b136bd829e387b7036
BoFengOne <1456065693@qq.com>
2019-12-14 21:44:20 +0800

    '代码控制路由'

---
+++ vue_cli2_project_01/src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>欢迎来到About!!!</h1>
+    <img src="../assets/girl.jpg">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+   
+</style>
