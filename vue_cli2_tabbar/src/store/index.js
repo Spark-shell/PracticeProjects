285c8549f22ffda7c3544a6caa99188d6e43d299
BoFengOne <1456065693@qq.com>
2019-12-15 20:30:21 +0800

    vuex mutations的响应式数据操作

--- vue_cli2_tabbar/src/store/index.js
+++ vue_cli2_tabbar/src/store/index.js
@@ -11,8 +11,6 @@ const store = new Vuex.Store({
       age: 12,
       addr: '甘肃庆阳宁县',
       phone: '1889391054*',
-      subject:'',
-      school:'',
     }
   },
   mutations: {      //mutations：前面讲到的都是如何获取state的数据，那如何把数据存储到state中呢？在 Vuex store 中，实际改变 状态(state) 的唯一方式是通过 提交(commit) 一个 mutation。　　mutations下的函数接收state作为参数，接收一个叫做payload（载荷）的东东作为第二个参数，这个东东是用来记录开发者使用该函数的一些信息，比如说提交了什么，提交的东西是用来干什么的，包含多个字段，所以载荷一般是对象（其实这个东西跟git的commit很类似）还有一点需要注意：mutations方法必须是同步方法！
@@ -24,8 +22,17 @@ const store = new Vuex.Store({
       state.userInfo.addr = '?'
     },
     addInfos(state, params) {
-      this.state.userInfo.subject = params.subject
-      this.state.userInfo.school = params.school
+      //1.添加数据的时候，如果state中之前没有的属性，赋值(增加)的数据不会刷新到页面
+      // this.state.userInfo.subject = params.subject
+      // this.state.userInfo.school = params.school
+      //2.要想刷新到页面，需要用这种方法
+      Vue.set(this.state.userInfo,'subject',params.subject)
+      Vue.set(this.state.userInfo,'school',params.school)
+      //3.同样删除的时候，如果用这种方法做不到响应式
+      // delete this.state.userInfo.age
+      //4.要做到响应式需要用这种方式
+      Vue.delete(this.state.userInfo,'age')
+
     }
   },
   getters: {        //对state的数据进行筛选，过滤，处理
