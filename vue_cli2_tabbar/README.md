f41f2321dd2da2c1be011e40b298fe81a882301c
BoFengOne <1456065693@qq.com>
2019-12-15 10:31:54 +0800

    tabbar案例基本架构搭建

---
+++ vue_cli2_tabbar/README.md
@@ -0,0 +1,21 @@
+# vue_cli2_tabbar
+
+> tabba练习2019-12-15
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ vue_cli2_tabbar/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+<style>
+  /*在style中引入css文件的方式*/
+  @import "assets/css/base.css";
+
+  #tab-bar {
+    display: flex;
+    background: #42b983;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    box-shadow: 0px -3px 1px rgba(66, 185, 131, 0.1);
+  }
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+</style>
