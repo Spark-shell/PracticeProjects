a340a474b832112635a7804fd7766772a01cfca3
BoFengOne <1456065693@qq.com>
2019-12-15 17:59:47 +0800

    Vuex添加使用vuex的基本使用

--- vue_cli2_tabbar/src/views/cart/Cart.vue
+++ vue_cli2_tabbar/src/views/cart/Cart.vue
@@ -1,12 +1,30 @@
 <template>
   <div >
     <h1>欢迎来到Cart</h1>
+    <h5>{{$store.state.userInfo}}</h5>
+    <button @click="add">补充信息</button>
+    <button @click="del">删除信息</button>
   </div>
 </template>
-
 <script>
   export default {
-    name: 'Cart'
+    name: 'Cart',
+    computed:{
+      userInfo(){
+        console.log($store.state )
+        return ($store.state.userInfo)
+      }
+    },
+    methods:{
+      add(){
+        console.log(this.$store.state)
+        this.$store.commit('fillInfo')
+      },
+      del(){
+        console.log(this.$store.state)
+        this.$store.commit('delInfo')
+      }
+    }
   }
 </script>
 <style scoped>
