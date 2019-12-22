efbad4cd141de10b0adc952abf8f8e62469b64d7
BoFengOne <1456065693@qq.com>
2019-12-22 13:00:39 +0800

    springboot 基本信息配置

--- SuperMall/src/main/java/com/gsau/project01/annocation/ApplicationAnnocation.java
+++
@@ -1,8 +0,0 @@
-package com.gsau.project01.annocation;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public @interface ApplicationAnnocation {
-
-}
