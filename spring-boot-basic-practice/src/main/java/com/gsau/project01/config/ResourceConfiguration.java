4b506db31d0bf46d5cdc1f8c864032412e9ba496
BoFengOne <1456065693@qq.com>
2019-12-24 21:53:17 +0800

    SpringBoot扩展模板视图解析器

--- spring-boot-basic-practice/src/main/java/com/gsau/project01/config/ResourceConfiguration.java
+++ spring-boot-basic-practice/src/main/java/com/gsau/project01/config/ResourceConfiguration.java
@@ -6,22 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
- /**
-  * @ Author: WangGQ
-  * @ Date: 2019/12/3 21:31
-  * @ Version: 1.0
-  * @ Description: 配置Spring Boot的Thymeleaf多个模板位置
-  */
+/**
+ * @ Author: WangGQ
+ * @ Date: 2019/12/3 21:31
+ * @ Version: 1.0
+ * @ Description: 配置Spring Boot的Thymeleaf多个模板位置
+ */
 @Configuration
 public class ResourceConfiguration {
-     /**
-      * @ Author: WangGQ
-      * @ Date: 2019/12/3 21:32
-      * @ Version: 1.0
-      * @ Description: 模板位置1
-      */
+    /**
+     * @ Author: WangGQ
+     * @ Date: 2019/12/3 21:32
+     * @ Version: 1.0
+     * @ Description: 模板视图解析器:模板位置1
+     */
     @Bean
     public SpringResourceTemplateResolver fileTemplateResolver() {
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
@@ -31,8 +33,24 @@ public class ResourceConfiguration {
         resolver.setCacheable(false);
         resolver.setOrder(0);
         resolver.setCharacterEncoding("UTF-8");
-
         resolver.setCheckExistence(true);
         return resolver;
+    }
+
+    @Bean
+    public FreeMarkerViewResolver secondTemplateResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setPrefix("");
+        resolver.setSuffix(".html");
+        resolver.setOrder(0);
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfig() {
+        FreeMarkerConfigurer freeMarkerConfig = new FreeMarkerConfigurer();
+        freeMarkerConfig.setTemplateLoaderPath("/WEB-INF/views/");
+        freeMarkerConfig.setDefaultEncoding("UTF-8");
+        return freeMarkerConfig;
     }
 }
