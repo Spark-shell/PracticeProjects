0ab9836b6361cb2ba0bb308eae0913b04577f15d
BoFengOne <1456065693@qq.com>
2019-12-26 21:43:37 +0800

    工厂模式模拟多例该单例

--- spring_base_learn/src/main/java/com/gsau/client/test1.java
+++ spring_base_learn/src/main/java/com/gsau/client/test1.java
@@ -5,7 +5,8 @@ import com.gsau.service.impl.AccountServiceImpl;
 
 public class test1 {
     public static void main(String[] args) {
-        AccountServiceImpl service= (AccountServiceImpl) BeanFactory.getBean("accountServiceImpl");
+//        AccountServiceImpl service= (AccountServiceImpl) BeanFactory.getBean("accountServiceImpl");
+        AccountServiceImpl service= (AccountServiceImpl) BeanFactory.createBean("accountServiceImpl");
         service.saveAccount();
     }
 }
--- spring_base_learn/src/main/java/com/gsau/factory/BeanFactory.java
+++ spring_base_learn/src/main/java/com/gsau/factory/BeanFactory.java
@@ -3,6 +3,9 @@ package com.gsau.factory;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -13,11 +16,26 @@ import java.util.Properties;
  */
 public class BeanFactory {
     public static Properties pros;
+    public static Map<String,Object> beans;
     static {
         try {
             pros = new Properties();
             InputStream in = BeanFactory.class.getClassLoader().getResourceAsStream("beans.properties");
             pros.load(in);
+            //----改造----start---
+            Enumeration enums=pros.keys();
+            beans=new HashMap<String,Object>();         //实例化容器
+            while (enums.hasMoreElements()){
+                try {
+                    String key    = (String) enums.nextElement();
+                    String keyPath=pros.getProperty(key);
+                    Object obj=Class.forName(keyPath).newInstance();
+                    beans.put(key,obj);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            //----改造----end---
         } catch (IOException e) {
             throw new ExceptionInInitializerError("读取配置文件失败");
         }
@@ -26,7 +44,7 @@ public class BeanFactory {
       * @ Author: WangGQ
       * @ Date: 2019/12/26 21:04
       * @ Version: 1.0
-      * @ Description: 获取bean
+      * @ Description: 获取bean,Class.forName(beanPath).newInstance()调用的是类的默认的构造方法，所以创建的bean是多例的
       */
     public static Object getBean(String beanName){
         Object obj = null;
@@ -37,5 +55,8 @@ public class BeanFactory {
                     e.printStackTrace();
             }
         return obj;
+    }
+    public static Object createBean(String beanName){
+        return beans.get(beanName);
     }
 }
