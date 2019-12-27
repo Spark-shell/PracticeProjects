39cbf84e912aa2c17c37d20d9c48442e717977e6
BoFengOne <1456065693@qq.com>
2019-12-27 10:28:42 +0800

    使用spring容器获取类对象

--- spring_base_learn/src/main/java/com/gsau/client/test1.java
+++ spring_base_learn/src/main/java/com/gsau/client/test1.java
@@ -1,12 +1,13 @@
 package com.gsau.client;
 
 import com.gsau.factory.BeanFactory;
+import com.gsau.factory.SpringBeanFactory;
 import com.gsau.service.impl.AccountServiceImpl;
 
 public class test1 {
     public static void main(String[] args) {
 //        AccountServiceImpl service= (AccountServiceImpl) BeanFactory.getBean("accountServiceImpl");
-        AccountServiceImpl service= (AccountServiceImpl) BeanFactory.createBean("accountServiceImpl");
+        AccountServiceImpl service= SpringBeanFactory.getBean("accountServiceImpl",AccountServiceImpl.class);
         service.saveAccount();
     }
 }
---
+++ spring_base_learn/src/main/java/com/gsau/factory/SpringBeanFactory.java
@@ -0,0 +1,35 @@
+package com.gsau.factory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ Description:  使用spring容器获取bean实例
+ * @ Date: 2019/12/27 9:57
+ * @ Author: wgq
+ * @ Version: 1.0
+ */
+public class SpringBeanFactory {
+    public static ApplicationContext ioc=null;
+     /**
+      * @ Description:  初始初始化Spring容器
+      * @ Date: 2019/12/27 10:22
+      * @ Author: wgq
+      * @ Version: 1.0
+      */
+    static {
+        ioc= new ClassPathXmlApplicationContext("SpringBeans.xml");
+    }
+     /**
+      * @ Description:  取容器中组件 
+      * @ Date: 2019/12/27 10:22
+      * @ Author: wgq
+      * @ Version: 1.0
+      */
+    public static Object getBean(String beanName){
+       return  ioc.getBean(beanName);
+    }
+    public static  < obj > obj getBean(String beanName,Class<obj> obj){
+        return  ioc.getBean(beanName,obj);
+    }
+}
