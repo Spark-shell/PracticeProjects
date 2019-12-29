99ca3d4a37232929302aed487cc97daa7e9380de
BoFengOne <1456065693@qq.com>
2019-12-29 19:35:22 +0800

    spring aop其他三种通知类型

--- spring_base_learn/aop/src/main/java/com/gsau/utils/Logger.java
+++ spring_base_learn/aop/src/main/java/com/gsau/utils/Logger.java
@@ -41,7 +41,6 @@ public class Logger {
      * 解决：
      *      Spring框架为我们提供了一个接口：ProceedingJoinPoint。该接口有一个方法proceed()，此方法就相当于明确调用切入点方法。
      *      该接口可以作为环绕通知的方法参数，在程序执行时，spring框架会为我们提供该接口的实现类供我们使用。
-     *
      * spring中的环绕通知：
      *      它是spring框架为我们提供的一种可以在代码中手动控制增强方法何时执行的方式。
      */
@@ -49,13 +48,9 @@ public class Logger {
         Object rtValue = null;
         try{
             Object[] args = pjp.getArgs();//得到方法执行所需的参数
-
             System.out.println("Logger类中的aroundPringLog方法开始记录日志了。。。前置");
-
             rtValue = pjp.proceed(args);//明确调用业务层方法（切入点方法）
-
             System.out.println("Logger类中的aroundPringLog方法开始记录日志了。。。后置");
-
             return rtValue;
         }catch (Throwable t){
             System.out.println("Logger类中的aroundPringLog方法开始记录日志了。。。异常");
