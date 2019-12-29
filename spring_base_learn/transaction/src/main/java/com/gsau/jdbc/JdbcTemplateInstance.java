c268cc45bbbc3d86db8fa9eaf8eeb8c238642cc6
BoFengOne <1456065693@qq.com>
2019-12-29 22:17:07 +0800

    spring 配置数据源,加入切面

--- spring_base_learn/transaction/src/main/java/com/gsau/jdbc/JdbcTemplateInstance.java
+++ spring_base_learn/transaction/src/main/java/com/gsau/jdbc/JdbcTemplateInstance.java
@@ -6,11 +6,9 @@ import com.gsau.factory.BeanFactory;
 import static java.lang.System.out;
 public class JdbcTemplateInstance {
     public static void main(String[] args) {
+        //放入容器中的组件如果实现了接口,那么从容器取的时候,第二个参数必须传接口的class类型,不然找不到
         IAccountDao jtplate=BeanFactory.getBean("accountDao",IAccountDao.class);
         jtplate.findAccountsByName("12");
         out.println("JdbcTemplateInstance.java--main--18-->"+jtplate.findAccountsByName("") );
-    }
-    public void test(){
-
     }
 }
